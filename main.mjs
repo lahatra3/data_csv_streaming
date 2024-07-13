b59568ba62641af9e482d1e761a88114c84420db
lahatra3 <lahatrap20.aps2a@gmail.com>
2024-07-14 00:42:17 +0300

    reduce highWaterMark to 7Mb

--- main.mjs
+++ main.mjs
@@ -8,7 +8,7 @@ const server = createServer((req, res) => {
     if (req.url === "/data.csv" && req.method === "GET") {
         const reader = createReadStream(STREAM_FILENAME, {
             encoding: "utf-8",
-            highWaterMark: 1024 * 1024 * 512,
+            highWaterMark: 1024 * 1024 * 7,
         });
 
         res.statusCode = 200;
